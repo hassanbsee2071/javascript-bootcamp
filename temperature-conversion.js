8744fc431ab2431e3d86f2f2dff201dde4a96950
syed.hassan <syed.hassan@spglobal.com>
2022-02-28 11:35:57 -0800

    Chapter 3, Build a temperature Converter

---
+++ temperature-conversion.js
@@ -0,0 +1,6 @@
+let fahrenheit = 32
+let celsius    = (fahrenheit - 32) * (5/9)
+console.log('Celsius Temparature is:', celsius, 'C')
+
+let kelvin = (fahrenheit - 32) * 5/9 + 273.15
+console.log('Kelvin Temparature is:', kelvin, 'K')
